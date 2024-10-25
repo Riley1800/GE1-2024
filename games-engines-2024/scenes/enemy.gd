ad6015b12acdeabcdd29e3f575acb65f175b7334
Riley1800 <c20426892@mytudublin.ie>
2024-10-25 16:07:59 +0100

    Week 6 - Improving enemy's relation to player

--- games-engines-2024/scenes/enemy.gd
+++ games-engines-2024/scenes/enemy.gd
@@ -4,35 +4,77 @@ extends MeshInstance3D
 
 var to_player
 var forw
+var theta
 var axis
+
+var q1:Quaternion
+var q2:Quaternion
+
+var t = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	var to_player = tank.global_position - global_position
+	to_player = tank.global_position - global_position
 	to_player = to_player.normalized()
 	
-	var forw = global_transform.basis.z
+	forw = global_transform.basis.z
 	
 	DebugDraw3D.draw_arrow(global_position, global_position + forw * 5, Color.AQUA, 0.1)
 	DebugDraw2D.set_text("enemy_to_player", to_player)
 	DebugDraw2D.set_text("forw", forw)
 	
-	var axis = to_player.cross(forw)
+	axis = to_player.cross(forw)
 	
-	var theta = acos(to_player.dot(forw))
-	
+	theta = acos(to_player.dot(forw))
+	axis = axis.normalized()
+	"""
 	DebugDraw2D.set_text("angle to the player", rad_to_deg(theta))
 	
 	DebugDraw2D.set_text("axis", axis)
 	
 	rotation = Vector3(0, -theta, 0)
+	"""
+	q2 = Quaternion(- axis, theta)
+	q1 = global_basis.get_rotation_quaternion()
+	t = 0
 	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	"""
+	to_player = tank.global_position - global_position
+	to_player = to_player.normalized()
 	
+	forw = global_transform.basis.z
+	
+	DebugDraw3D.draw_arrow(global_position, global_position + forw * 5, Color.AQUA, 0.1)
+	DebugDraw2D.set_text("enemy_to_player", to_player)
+	DebugDraw2D.set_text("forw", forw)
+	
+	axis = to_player.cross(forw)
+	
+	theta = acos(to_player.dot(forw))
+	
+	DebugDraw2D.set_text("angle to the player", rad_to_deg(theta))
+	
+	DebugDraw2D.set_text("axis", axis)
+	
+	rotation = Vector3(0, -theta, 0)
+	"""
+	
+	var q:Quaternion
+	
+	if t < 1.0:
+		t = t + delta
+	else:
+		t = 1.0
+	
+	DebugDraw2D.set_text("t", t)
+	q= q1.slerp(q2, t)
+	global_basis = Basis(q)
 	
 	pass
